2520b4fc519592af2ece7ae8e20b1be0701328e3
alkasah <alkasah485@gmail.com>
2024-04-07 03:36:33 -0700

    styling in css and JS

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-    if(email.value === ' ') {
-        error.innerHtml = 'Oops! Please add your email';
+const form = document.querySelector("form");
+const input = document.querySelector(".submitDocs");
+const spanErr = document.querySelector(".errorMsg");
 
-    }
+input.addEventListener("invalid", () => {
+    spanErr.classList.add("showError");
+})
+input.addEventListener("input", () => {
+    spanErr.classList.remove("showError")
+})
+form.addEventListener("submit", (evt) => {
+    console.log(evt)
+    evt.preventDefault()
+     alert("Your form has been submitted, Thank you")
+     input.value = ""
 })
